25e0278921180b7cd69b441e7f1c81de05ee1b3a
Michael Drozdov <mdrozdov94@gmail.com>
2019-11-10 23:07:10 -0500

    instantiated user inputs and output script via switch statement

--- primes2.cpp
+++ primes2.cpp
@@ -2,12 +2,40 @@
 #include "primes.h"
 
 int main()
-{
+{    
+    int num;
+    char selection_var;
+    
+    do
+    {    
+        std::cout << "F : Find the nth prime number" << std::endl <<
+        "T : Test a value to see if its prime" << std::endl <<
+        "Q : Terminate the program" << std::endl;
 
-std::cout << isPrime(19);
+        std::cin >> selection_var;
+
+        switch(selection_var)
+        {
+            case 'F':
+                std::cout << std::endl << "Enter the Nth prime number you'd like to find: " << std::endl;
+                std::cin >> num;
+                std::cout << std::endl << 
+                Prime(num) << std::endl << std::endl;
+                continue;
+            case 'T':
+                std::cout << std::endl << "Enter a number to test if prime: " << std::endl;
+                std::cin >> num;
+                std::cout << std::endl << 
+                isPrime(num) << std::endl << std::endl;
+                continue;
+            case 'Q':
+                selection_var = 'Q';
+        }
+        
+    } while (selection_var != 'Q');
 
 
-std::cout << std::endl << Prime(7);
+
 
 
 
